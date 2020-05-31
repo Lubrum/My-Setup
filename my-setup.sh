c4c27105744ebefff4ec26a7a60f3c5bc3a5bd8c
Lubrum <lucianobrum18@gmail.com>
2020-05-31 13:34:00 -0300

    .sh updated with other tools and snap packages

--- my-setup.sh
+++ my-setup.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
+cd $HOME
 sudo apt-get update
 
-echo 'Installing  git' 
+echo 'Installing wget and ca-certificates'
+sudo apt-get install wget ca-certificates
+
+################ Version Control ################
+echo 'Installing Git' 
 sudo apt-get install git -y
 
 echo "Provide Git user.name"
@@ -12,61 +17,22 @@ echo "Provide Git user.email"
 read git_user_email
 git config --global user.email "$git_user_email"
 
+################ For R development ################
+echo 'installing R 4.0 (Ubuntu 18.04.4 LTS - Bionic Beaver)'
+sudo add-apt-repository 'deb https://cloud.r-project.org/bin/linux/ubuntu bionic-cran40/'
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
-sudo add-apt-repository 'deb https://cloud.r-project.org/bin/linux/ubuntu bionic-cran35/'
 sudo apt update
+sudo apt install r-base r-base-core r-recommended r-base-dev -y
 
-echo 'Installing  R-base, gdebi-core, build-essential' 
-sudo apt install r-base -y
-sudo apt install gdebi-core -y
-sudo apt install make build-essential -y
-
-echo 'Installing  openvpn (18.04.4 LTS)' 
-wget -O - https://swupdate.openvpn.net/repos/repo-public.gpg|apt-key add -
-echo "deb http://build.openvpn.net/debian/openvpn/stable bionic main" > /etc/apt/sources.list.d/openvpn-aptrepo.list
-
-echo 'Installing  java 11 jdk' 
-sudo apt install default-jdk -y
-
-echo 'Installing  libgdal-dev (R)' 
+echo 'Installing libgdal-dev (R)' 
 sudo apt install libgdal-dev -y
-
-echo 'Installing  openssh-server' 
-sudo apt install openssh-server -y
-
-sudo add-apt-repository ppa:linuxuprising/java
-sudo apt-get update
-
-echo 'Installing  java 11 jre' 
-sudo apt install openjdk-11-jre-headless -y
-sudo apt autoremove
-
-echo 'Installing google chrome'
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo dpkg -i google-chrome-stable_current_amd64.deb
-
-echo 'Installing  libnss3 (R)' 
-sudo apt-get install --reinstall libnss3 -y
-
-echo 'Installing postgresql postgresql-contrib' 
-sudo apt install postgresql postgresql-contrib -y
-sudo apt-get install wget ca-certificates
-wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
-sudo apt-get update
-
-echo 'Installing pgadmin4 pgadmin4-apache2' 
-sudo apt install pgadmin4 pgadmin4-apache2 -y
-
-sudo apt update && sudo apt -y upgrade
-echo 'Installing postgresql-11' 
-sudo apt -y install postgresql-11
-sudo -u postgres psql
-
-echo 'Installing  cargo (R)' 
+echo 'Installing libnss3 (R)' 
+sudo apt-get install libnss3 -y
+echo 'Installing cargo (R)' 
 sudo apt-get install cargo -y
-echo 'Installing  libavfilter-dev (R)' 
+echo 'Installing libavfilter-dev (R)' 
 sudo apt-get install libavfilter-dev -y
-echo 'Installing  libudunits2-dev (R)' 
+echo 'Installing libudunits2-dev (R)' 
 sudo apt-get install libudunits2-dev -y
 sudo add-apt-repository -y ppa:cran/poppler
 sudo apt-get update
@@ -75,10 +41,61 @@ sudo apt-get install libpoppler-cpp-dev -y
 echo 'Installing libsdl2-2.0-0 (R)' 
 sudo apt-get install libsdl2-2.0-0 -y
 
+echo 'Download RStudio 1.3.959 (2020-05-28)'
+wget https://download1.rstudio.org/desktop/bionic/amd64/rstudio-1.3.959-amd64.deb
+sudo dpkg -i $HOME/rstudio-1.3.959-amd64.deb
+
+################ Private network tools ################
+echo 'Installing openvpn (Ubuntu 18.04.4 LTS - Bionic Beaver)' 
+wget -O - https://swupdate.openvpn.net/repos/repo-public.gpg|apt-key add -
+echo "deb http://build.openvpn.net/debian/openvpn/stable bionic main" > /etc/apt/sources.list.d/openvpn-aptrepo.list
+
+################ For Java development ################
+echo 'Installing java 11 jdk' 
+sudo apt install default-jdk -y
+
+sudo add-apt-repository ppa:linuxuprising/java
+sudo apt-get update
+
+echo 'Installing java 11 jre' 
+sudo apt install openjdk-11-jre-headless -y
+sudo apt autoremove
+
+echo 'Installing java 8 jdk' 
+sudo apt install openjdk-8-jdk
+sudo update-alternatives --config java
+
+################ ssh ################
+echo 'Installing openssh-server' 
+sudo apt install openssh-server -y
+
+# only works after installing git
+echo 'Generating ssh key'
+ssh-keygen -t rsa -b 4096 -C $git_config_user_email
+ssh-add ~/.ssh/id_rsa
+cat ~/.ssh/id_rsa.pub | xclip -selection clipboard
+
+################ For dependency management ################
+echo 'Installing maven' 
+sudo apt install maven -y
+
+echo 'Installing google chrome'
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+
+################ postgresql and pgadmin4 (database) ################
+echo 'Installing pgadmin4 pgadmin4-apache2' 
+sudo apt install pgadmin4 pgadmin4-apache2 -y
+
+echo 'Installing postgresql-11' 
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+sudo apt update && sudo apt -y upgrade
+sudo apt -y install postgresql-11
+
+################ Jekyll Dependencies (Ruby) ################
 echo 'Installing Ruby' 
 sudo apt-get update
 sudo apt-get install ruby -y
-sudo apt autoremove
 
 echo 'Installing ruby-dev' 
 sudo apt-get install ruby-dev -y
@@ -87,18 +104,11 @@ sudo gem install jekyll
 sudo bundle update
 sudo bundle install
 
+################ Automated mail tools ################
 echo 'Installing mailutils' 
 sudo apt install mailutils -y
 
-echo 'Installing kazam' 
-sudo apt-get install kazam -y
-sudo apt-get upgrade kazam -y
-
-echo 'Installing java 8 jdk' 
-sudo apt install openjdk-8-jdk
-sudo update-alternatives --config java
-
-# ajustes para viabilizar hibernação no Ubuntu 18.04.4
+# hibernation on Ubuntu 18.04.4
 #grep swap /etc/fstab
 #sudo nano /etc/default/grub
 #sudo nano /etc/initramfs-tools/conf.d/resume
@@ -108,10 +118,35 @@ sudo update-alternatives --config java
 #sudo update-grub
 #sudo systemctl hibernate
 
-# only works after installing git
-echo 'Generating ssh key'
-ssh-keygen -t rsa -b 4096 -C $git_config_user_email
-ssh-add ~/.ssh/id_rsa
-cat ~/.ssh/id_rsa.pub | xclip -selection clipboard
+echo 'Installing net-tools for ifconfig and net tools D:'
+sudo apt install net-tools -y
 
-# Pendente: RStudio, Eclipse, Android Studio, Postman, Shotcut, Shotwell, KolourPaint
+# If OS does not have Snap... 
+#sudo apt update
+#sudo apt install snapd -y
+
+# only works if system has Snap (Snap is already installed in Ubuntu 16.04+ versions)
+################ IDE for development (Java in my case) ################
+echo 'Installing Eclipse (snap)'
+sudo snap install --classic eclipse
+
+################ API tests ################
+echo 'Installing Postman (snap)'
+sudo snap install postman
+
+################ IDE for mobile development (Java and Kotlin in my case) ################
+echo 'Installing Android Studio (snap)'
+sudo snap install android-studio --classic
+
+################ General purpose tools ################
+echo 'Installing kazam (screen recorder)' 
+sudo apt-get install kazam -y
+
+echo 'Installing Shotcut (snap) (video editor)'
+sudo snap install shotcut --classic
+
+echo 'Installing ShotWell (snap) (image editor)'
+sudo snap install shotwell-adam --edge
+
+echo 'Installing kolourpaint (an alternative for known paint from Windows'
+sudo apt install kolourpaint
